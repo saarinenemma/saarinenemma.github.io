540070c806a92ce998ded2d9e7a90290ca094b98
saarinenemma <81643935+saarinenemma@users.noreply.github.com>
2021-12-17 16:23:27 -0500

    Update App.tsx

--- SaarinenEmma/src/App.tsx
+++ SaarinenEmma/src/App.tsx
@@ -38,39 +38,26 @@ setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonTabs>
+      <IonReactRouter>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
+          <Route exact path="/dashboard">
             <Tab1 />
           </Route>
-          <Route exact path="/tab2">
+          <Route exact path="/clients">
             <Tab2 />
           </Route>
-          <Route path="/tab3">
+          <Route exact path="/clientOverview">
             <Tab3 />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/dashboard" />
+          </Route>
+          <Route>{/* Default catch-all route at end. */}
+          <Tab3 />
           </Route>
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+      </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
