14de2f40b025becadfaf5fb60deb7396d30b4ed0
saarinenemma <81643935+saarinenemma@users.noreply.github.com>
2022-01-04 04:55:39 -0500

    Update ProjectsPage.tsx

--- SaarinenEmma/src/pages/ProjectsPage.tsx
+++ SaarinenEmma/src/pages/ProjectsPage.tsx
@@ -329,6 +329,7 @@ const [currentClient,setCurrentClient]=useState(new Client(
   function changeBackground(e) {
     e.target.style.fontWeight='bolder';
   }
+  
   function fixBackground(e){e.target.style.fontWeight='lighter'}
   function updateLinks(Links:string[]){
   
@@ -337,7 +338,7 @@ const [currentClient,setCurrentClient]=useState(new Client(
       const currentLink=Links[i];
       if (currentLink==Links[Links.length-1]){templinks.push( <IonText class = "curlinks" onClick={() => { window.location.assign('/'+currentLink); }}>{Links[i] }</IonText>);}
       else{
-    templinks.push( <IonText class = "links" onClick={() => { window.location.assign('/'+currentLink); }} onMouseEnter={changeBackground}onMouseLeave={fixBackground}>{Links[i] }</IonText>);}
+    templinks.push( <IonText class = "links" onClick={() => { window.location.assign('/'+currentLink); }} /*onMouseEnter={changeBackground}onMouseLeave={fixBackground}*/>{Links[i] }</IonText>);}
     templinks.push(<IonText>{" | "}</IonText> );
     }
     templinks.pop();
@@ -355,7 +356,7 @@ const [currentClient,setCurrentClient]=useState(new Client(
           <IonTitle class="Title2">Emma Saarinen</IonTitle>
           <IonText>Researcher, Tutor, Developer</IonText>
           </IonCol>
-          <IonCol  >{updateLinks(links)}</IonCol>
+          <IonCol  >updateLinks(links)</IonCol>
           </IonRow>
           </IonGrid>
         </IonToolbar>
